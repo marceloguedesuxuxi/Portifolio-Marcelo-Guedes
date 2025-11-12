e8b88f0ae87f92696f9ec3a769d2d46ae989babe
Marcelo Guedes <marceloguedes.s@hotmail.com>
2025-11-12 19:43:15 -0300

    Portfolio Marcelo Guedes

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["UX/UI Designer", "Product Designer" , "Web Designer" , "Front-End Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
